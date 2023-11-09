d2aad02fd2163180f5fb61a5f124aaa6720f73e4
ghulammujtabaadill123 <ghulam.mujtabadil001@gmail.com>
2023-11-09 22:35:52 +0500

    Exceptions Handling, Local SubPrograms

--- Procedures and Functions/Functions_11_08_2023.sql
+++ Procedures and Functions/Functions_11_08_2023.sql
@@ -101,6 +101,78 @@ end;
 select * from high_salary_emp;
 /
 --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+----------------<<<<<<< OverLoading >>>>>>>>>>>---------------------------------
+declare
+    procedure insert_employees( p_emps employees%rowtype) is
+        c_emps employees%rowtype;
+    function get_employees ( emp_id employees.employee_id%type) return employees%rowtype is
+        begin
+            select * into c_emps from employees where employee_id=emp_id;
+            return c_emps;
+        end;
+    ----- get the employee based on the email..
+    function get_employees ( emp_email employees.email%type) return employees%rowtype is
+        begin
+            select * into c_emps from employees where email=emp_email;
+            return c_emps;
+        end;
+    ----Procedure begin Block
+    begin
+        c_emps := get_employees(p_emps.email);
+        insert into high_salary_emp values c_emps;
+    end;
+begin
+    for i in (select * from employees)
+    loop
+        if i.salary > 15000
+        then
+            insert_employees(i);
+        end if;
+    end loop;
+end;
+/
+select * from high_salary_emp;
+/
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+----------------<<<<<<< Handling Exceptions >>>>>>>>>>>-------------------------
+create or replace function get_emp( emp_id employees.employee_id%type) 
+    return employees%rowtype is
+    emp employees%rowtype;
+    begin
+        select * into emp from employees where employee_id = emp_id;
+        return emp;
+        exception
+            when no_data_found
+            then
+                dbms_output.put_line(' No Employee exist having an Employee_id: '|| emp_id);
+                raise no_data_found;
+            when OTHERS
+                then 
+                    dbms_output.put_line('Unexpected Error Occured...');
+                    return null;
+    end;
+/
+set serveroutput on;
+declare
+    c_emps employees%rowtype;
+begin
+    dbms_output.put_line(' Information of the Employee are .......');
+    c_emps := get_emp(10);
+    dbms_output.put_line('First Name: ' || c_emps.first_name);
+    dbms_output.put_line('last Name: ' || c_emps.last_name);
+    dbms_output.put_line('Salary : ' || c_emps.salary);
+    dbms_output.put_line('First Hire Date: ' || c_emps.hire_date);
+end;
+/
+ 
+
+
+
+
+
+        
             
     
     
