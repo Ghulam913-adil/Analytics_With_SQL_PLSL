d29d927d2a5ffbecc54c7b249c6b265e73ba6458
Ghulam Mujtaba Adil <63853407+Ghulam913-adil@users.noreply.github.com>
2023-11-10 23:43:17 +0500

    Update Package_11-10-2023.sql
    additional  sql command to commit the changes...

--- Packages/Package_11-10-2023.sql
+++ Packages/Package_11-10-2023.sql
@@ -50,5 +50,7 @@ BEGIN
     High_Sal_Emp.process_high_salary_emp;
 END;
 /
+    commit;
+/
 select * from high_salary_emp;
 ----------------------------------------------------------------------------------------------------------------------
