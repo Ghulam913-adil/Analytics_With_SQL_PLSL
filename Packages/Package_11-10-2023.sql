ae611530ca97d624a3928ff56a072c6feb22325e
ghulammujtabaadill123 <ghulam.mujtabadil001@gmail.com>
2023-11-14 00:16:40 +0500

    records and packages

--- Packages/Package_11-10-2023.sql
+++ Packages/Package_11-10-2023.sql
@@ -36,6 +36,64 @@ CREATE OR REPLACE PACKAGE BODY employee_info_pkg IS
 
 END employee_info_pkg;
 
+---------------------------------------------------------------------------------------------------------------------
+-------------------- Record with Package Employee Education---------------------------------------------
+create or replace PACKAGE employee_info_pkg2 IS
+  -- Define the record types
+  TYPE t_edu IS RECORD (
+    primary_school    VARCHAR2(100),
+    high_school       VARCHAR2(100),
+    university        VARCHAR2(100),
+    uni_graduate_date DATE
+  );
+
+  TYPE t_emp IS RECORD (
+    first_name       VARCHAR2(50),
+    last_name        employees.last_name%TYPE,
+    salary           employees.salary%TYPE NOT NULL DEFAULT 1000,
+    hire_date        DATE,
+    dept_id          employees.department_id%TYPE,
+    department       departments%ROWTYPE,
+    education        t_edu
+  );
+
+  -- Declare the procedure to fetch and display employee information
+  PROCEDURE get_employee_info(employee_id_param IN employees.employee_id%TYPE);
+END employee_info_pkg2;
+/
+CREATE OR REPLACE PACKAGE BODY employee_info_pkg2 IS
+  -- Implement the procedure
+  PROCEDURE get_employee_info(employee_id_param IN employees.employee_id%TYPE) IS
+    r_emp t_emp; -- Declare the record variable
+
+  BEGIN
+    SELECT first_name, last_name, salary, hire_date, department_id 
+    INTO r_emp.first_name, r_emp.last_name, r_emp.salary, r_emp.hire_date, r_emp.dept_id 
+    FROM employees 
+    WHERE employee_id = employee_id_param;
+  
+    SELECT * 
+    INTO r_emp.department 
+    FROM departments 
+    WHERE department_id = r_emp.dept_id;
+  
+    r_emp.education.high_school       := 'Beverly Hills';
+    r_emp.education.university        := 'Oxford';
+    r_emp.education.uni_graduate_date := TO_DATE('01-JAN-13', 'DD-MON-RR');
+  
+    dbms_output.put_line(r_emp.first_name || ' ' || 
+                         r_emp.last_name || ' earns ' || 
+                         r_emp.salary || ' and was hired on: ' || 
+                         TO_CHAR(r_emp.hire_date, 'DD-MON-YY'));
+    dbms_output.put_line('She graduated from ' || 
+                         r_emp.education.university || 
+                         ' at ' ||  
+                         TO_CHAR(r_emp.education.uni_graduate_date, 'DD-MON-YY'));
+    dbms_output.put_line('Her Department Name is: ' || r_emp.department.department_name);
+  END get_employee_info;
+
+END employee_info_pkg2;
+/
 
 
 
